0a36f3638bc76a69f7df9eedecb9f92e273c0950
Ayush Gangani <155182044+AG3905@users.noreply.github.com>
2026-09-06 23:29:10 +0530

    Time: 19 ms (69.08%), Space: 54.4 MB (38.68%) - LeetHub - Time: O(n * m), Space: O(n * m)

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        int[][] dp = new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            dp[i][0] = 1;
+        }
+        for(int j=1;j<=m;j++){
+            dp[0][j] = 0;
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s.charAt(i-1)==t.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1]+dp[i-1][j];
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
