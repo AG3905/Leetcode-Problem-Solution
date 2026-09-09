73b05acfa75999da6abf6ebd453d81b7efcabd2d
Ayush Gangani <155182044+AG3905@users.noreply.github.com>
2026-09-09 19:49:57 +0530

    Time: 1 ms (99.36%), Space: 43 MB (11.54%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    private static final long[] pow1000 = { 1000L, 1000000L, 1000000000L, 1000000000000L, 1000000000000000L, 1000000000000000000L };
+
+    public long countCommas(long n) {
+        int k = 0;        
+        for (long p : pow1000) if (n >= p) k++;
+        
+        return k * (n + 1) - (pow1000[k] - 1000) / 999;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
